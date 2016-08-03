4a0611637f24a409e3fafb3abcc89de0e79fe728
Tryston Maris <tmaris@student.neumont.edu>
2016-08-03 17:17:01 -0600

    Making Pieces with movements.

---
+++ Chess/ConsolChess/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ChessModel;
+using ChessController;
+using System.IO;
+
+namespace ConsolChess
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Program starting...");
+            Board board = new Board();
+            //board.Initialize("C:\\Chess\\Chess\\ConsolChess\\Data\\Initialization.txt");
+            //Console.WriteLine(board.ToString());
+            //board.TakeSquare("h1 d2", true);
+            
+            board.InitializeReaderTest("C:\\Chess\\Chess\\ConsolChess\\Data\\IOTest.txt");
+            Console.WriteLine("Program ending...");
+        }
+    }
+}
